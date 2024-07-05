2a07dc37903eebfacd2640535b01fc30c353e255
Matthias Giannesini <matthias.giannesini@gmail.com>
2024-07-05 15:13:05 +1000

    Python Catchup

---
+++ py-catchup.py
@@ -0,0 +1,5 @@
+import sys
+
+# print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+# print(sys.version)
