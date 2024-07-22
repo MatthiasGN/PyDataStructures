a640c0b2a9977c591332ae8fdb94b25a0cda53e4
Matty Genius <mattygenius2@gmail.com>
2024-07-22 18:05:40 +1000

    DS3 Chapter 3 work.

--- Runestone-DS3/chapter3.py
+++ Runestone-DS3/chapter3.py
@@ -2,4 +2,27 @@
 
 '''3 Data Types
 Looking at data types once more. Most of my study will be done on the readme but anything extra can go here.
+
+What different types of data structures exist in Python?
+
+Primitive:
+- Characters
+- Integers
+- Floats
+- Booleans
+
+Non-Primitive (built-ins):
+- Lists
+- Dictionaries
+- Sets
+- Tuples
+
+Non-Primitive (user-defined):
+- Queues
+- Deques
+- Stacks
+- LinkedLists
+- Trees
+- etc
+
 '''
