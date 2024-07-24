7f7044b755abb7a08e51af69b5938922b21c7917
Matthias Giannesini <matthias.giannesini@gmail.com>
2024-07-25 08:28:17 +1000

    DS3 Chapter 3 - Stacks

--- Runestone-DS3/chapter3.py
+++ Runestone-DS3/chapter3.py
@@ -1,7 +1,7 @@
 
 
 '''3 Data Types
-Looking at data types once more. Most of my study will be done on the readme but anything extra can go here.
+When we give an abstract data type (ADT) a physical implementation, we can now refer to that implementation as a data structure.
 
 What different types of data structures exist in Python?
 
@@ -18,9 +18,9 @@ Non-Primitive (built-ins):
 - Tuples
 
 Non-Primitive (user-defined):
+- Stacks
 - Queues
 - Deques
-- Stacks
 - LinkedLists
 - Trees
 - etc
@@ -41,7 +41,7 @@ A useful idea to consider is that stacks essentially reverse the order of insert
 I.e. if you looped through a list and added each item into a stack, the stack automatically has reversed the order
 of that list.
 
-A real example is the idea of your current tab history. Your current webpage is on the top of the stack, but you
+A good example is the idea of your current tab history. Your current webpage is on the top of the stack, but you
 can hit back as many times as you want to move in reverse order through the webpages.
 
 Stacks are ordered LIFO.
@@ -50,3 +50,44 @@ The really important thing here is to note how it's implemented in Python. The e
 actually the first element in the Python List. This makes it much more efficient to push and pop items from the top of
 the stack (i.e. the end of the list, which is less memory intensive).
 '''
+
+class Stack:
+
+    def __init__(self):
+        self.ls = []
+
+    def __repr__(self):
+        return f"Stack({self.ls})"
+
+    def push(self, obj: any):
+        self.ls.append(obj)
+
+    def pop(self):
+        return self.ls.pop()
+
+    def is_empty(self):
+        return len(self.ls) < 1
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Stack is empty")
+        return self.ls[-1]
+    
+    def size(self):
+        return len(self.ls)
+    
+# Four principles of OOP:
+# Encapsulation - both data and methods are contained within the same object. I.e. getters and setters
+# Inheritance - classes can inherit variables and methods from parent classes
+# Polymorphism - classes can be treated as instances of their parent classes. E.g. Dog can be input to a function that only accepts Animal
+# Abstraction - complex ideas can be represented in a number of different, simpler ways. E.g. a stack, an ADT, can be implemented in Python as a data structure in multiple ways.
+
+s = Stack()
+s.push(1)
+s.peek()
+print(s)
+print(s.pop())
+s.push(1)
+s.push(2)
+s.push(3)
+print(s)
